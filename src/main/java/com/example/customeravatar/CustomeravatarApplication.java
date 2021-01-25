ecf35b0f4415430baa6496547567c3a96a6cb037
zefanyadavids <zefanya.david@ti.ukdw.ac.id>
2021-01-25 17:25:24 +0700

    initial commit

---
+++ src/main/java/com/example/customeravatar/CustomeravatarApplication.java
@@ -0,0 +1,13 @@
+package com.example.customeravatar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomeravatarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomeravatarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/customeravatar/CustomeravatarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.customeravatar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomeravatarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
